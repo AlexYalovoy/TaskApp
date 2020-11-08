96cf1ca58a8aa4ca7caed0d53587e5153f9fa8f3
YalovyyAlex <YalovyyAlex@gmail.com>
2020-11-08 18:03:03 +0200

    add read endpoints for users and tasks

--- src/index.js
+++ src/index.js
@@ -8,14 +8,40 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get('/users', (req, res) => {
+    User.find({})
+      .then((users) => res.send(users))
+      .catch(() => res.status(500).send());
+});
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+
+    User.findOne({ _id })
+      .then((user) => (user ? res.send(user) : res.status(404).send()))
+      .catch(() => res.status(500).send());
+});
+
+app.get('/tasks', (req, res) => {
+    Task.find({})
+      .then((tasks) => res.send(tasks))
+      .catch(() => res.status(500).send());
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Task.findById(_id)
+      .then((task) => (task ? res.send(task) : res.status(404).send()))
+      .catch(() => res.status(500).send());
+});
+
 app.post('/users', (req, res) => {
     const user = new User(req.body);
     user
       .save()
       .then(() => res.status(201).send(user))
-      .catch((err) => {
-        res.status(400).send(err);
-      });
+      .catch((err) => res.status(400).send(err));
 });
 
 app.post('/tasks', (req, res) => {
