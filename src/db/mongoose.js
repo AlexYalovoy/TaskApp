9beb93ad700e923799eb7dba5c1232abd8c5a76c
YalovyyAlex <YalovyyAlex@gmail.com>
2020-11-08 16:31:10 +0200

    add mongoose

---
+++ src/db/mongoose.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid')
+      }
+    }
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number')
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password contains "password"');
+      }
+    }
+  }
+});
+
+const Task = mongoose.model('Task', {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  }
+});
+
+const newTask = new Task({ description: "learn structuring rest api" });
+const newUser = new User({ name: "Joshua", email: 'joshua@gmail.com', age: 20 });
+
+newTask.save().then(() => {
+  console.log('saved');
+}).catch((error) => {
+  console.log('error', error);
+});
